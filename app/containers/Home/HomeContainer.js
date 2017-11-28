a7e4ff4165f322448845c4ec7f05cd9ba6dbd628
tlabna <tlabna@users.noreply.github.com>
2017-11-28 13:30:31 -0500

    feat: Add Home + Navigation components with css styling
    - composing shared styles to optimize future refactoring
    - Splitting container and presentational components to optimize future refactoring (ex. hooking redux to home container)

---
+++ app/containers/Home/HomeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Home } from 'components'
+
+class HomeContainer extends React.Component {
+  render () {
+    return (
+      <Home />
+    )
+  }
+}
+
+export default HomeContainer
--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Navigation } from 'components'
+import { container, innerContainer } from './styles.css'
 
 class MainContainer extends React.Component {
   render () {
     return (
-      <p>{'Hello World!'}</p>
+      <div className={container}>
+        <Navigation isAuthed={true} />
+        <div className={innerContainer}>
+          {this.props.children}
+        </div>
+      </div>
     )
   }
+}
+
+MainContainer.propTypes = {
+  children: PropTypes.object.isRequired
 }
 
 export default MainContainer
