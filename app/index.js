72baa1e8a3ad11105a00f139e530344cb0438f6d
tlabna <tlabna@users.noreply.github.com>
2017-12-12 12:25:37 -0500

    chore: Add code to enable redux developer tools for chrome extension

--- app/index.js
+++ app/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import getRoutes from './config/routes'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import users from 'redux/modules/users'
 import thunk from 'redux-thunk'
 import restricted from 'helpers/restricted'
 
 // create state tree from suplied reducers
-const store = createStore(users, applyMiddleware(thunk))
+const store = createStore(users, compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f // start extension or return first argument
+))
+
 console.log(store)
 console.log(store.getState())
 
